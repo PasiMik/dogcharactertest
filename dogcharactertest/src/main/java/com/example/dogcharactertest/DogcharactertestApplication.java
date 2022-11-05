cb74260fdf177b5217ebe23e9be5844737dfc989
Pasi Mikkola <pasimikkola111@gmail.com>
2022-11-05 19:35:22 +0200

    Added delete, save and edit functions under ADMIN account

--- dogcharactertest/src/main/java/com/example/dogcharactertest/DogcharactertestApplication.java
+++ dogcharactertest/src/main/java/com/example/dogcharactertest/DogcharactertestApplication.java
@@ -7,7 +7,9 @@ import org.springframework.context.annotation.Bean;
 
 import com.example.dogcharactertest.domain.Dog;
 import com.example.dogcharactertest.domain.DogRepository;
+import com.example.dogcharactertest.domain.User;
 import com.example.dogcharactertest.domain.UserRepository;
+
 
 @SpringBootApplication
 public class DogcharactertestApplication {
@@ -21,6 +23,11 @@ public class DogcharactertestApplication {
 		return (args) -> {
 			
 			repository.save(new Dog("25.10.2022","Australian shepherd"," Frost Lake's Future","FI12345/22","+2","+2","-1","+1","+1","+1","+1","+3","+++",142));
+			
+			User user1 = new User("user", "$2a$10$aeYF.z4171UTtrGFvh/9aOOkk0VPj8Sq0CFRYzMSpuMTyBX8SfRnm", "USER");
+			User user2 = new User("admin", "$2a$10$GQOA2HC5rJ0HjARfGcqZUufFdluGJ4sSOR6Z6eC2swOlN47I74nGW", "ADMIN");
+			urepository.save(user1);
+			urepository.save(user2);
 		};
 		
 	}
--- dogcharactertest/src/main/java/com/example/dogcharactertest/web/DogController.java
+++ dogcharactertest/src/main/java/com/example/dogcharactertest/web/DogController.java
@@ -1,6 +1,7 @@
 package com.example.dogcharactertest.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,6 +30,7 @@ public class DogController {
 		return "results";
 	}
 	
+	@PreAuthorize("hasAuthority('ADMIN')")
 	@GetMapping("/add_dog")
 	public String addDog(Model model){
 		model.addAttribute("dog", new Dog());
@@ -41,12 +43,14 @@ public class DogController {
 		return "redirect:testresults";
 	}
 	
+	@PreAuthorize("hasAuthority('ADMIN')")
 	@GetMapping("/delete/{id}")
 	public String deleteDog(@PathVariable("id") long id, Model model) {
 	repository.deleteById(id);
 	return "redirect:../testresults";
 	}
 	
+	@PreAuthorize("hasAuthority('ADMIN')")
 	@GetMapping("/editdog/{id}")
 	public String edit(@PathVariable("id") long id, Model model){
 		model.addAttribute("dog", repository.findById(id));
