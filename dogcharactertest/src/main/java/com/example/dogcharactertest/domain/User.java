1d5553542f9a09f03680f90a96286f59ec78cd1d
Pasi Mikkola <pasimikkola111@gmail.com>
2022-11-12 17:52:14 +0200

    application.properties change

--- dogcharactertest/src/main/java/com/example/dogcharactertest/domain/User.java
+++ dogcharactertest/src/main/java/com/example/dogcharactertest/domain/User.java
@@ -7,6 +7,7 @@ import javax.persistence.*;
 @Entity
 @Table(name="usertable")
 public class User {
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id", nullable = false, updatable = false)
